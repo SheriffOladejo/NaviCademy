05990b8140bed211ef45720f580a636ab761ad81
Thomas Sorensen <tsoerensen@me.com>
2023-11-21 21:59:13 +0100

    Initial Commit

---
+++ NaviCademy/NaviCademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaviCademyApp.swift
+//  NaviCademy
+//
+//  Created by Thomas Sørensen on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct NaviCademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
